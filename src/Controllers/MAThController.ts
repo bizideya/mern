00c9b8b4a47902dd9c4ac8f0a66c47106cd11d5f
bizideya <eyabizidd@gmail.com>
2023-02-25 12:31:17 +0100

    creat project wint nestjs

--- src/Controllers/MAThController.ts
+++
@@ -1,7 +0,0 @@
-import express, { Request, Response,NextFunction} from 'express'
-const app = express()
-import {ValidateParms} from '../Middleware/ValidateParms';
-import{Handler }from '../Services/MathService';
-
-app.use("/calc",ValidateParms,Handler);
-
