09dec501ea990d6a7081ffefc7fb9a25110a11f3
bizideya <eyabizidd@gmail.com>
2023-02-25 14:11:28 +0100

    update

---
+++ src/config/config.ts
@@ -0,0 +1,9 @@
+import { MysqlConnectionOptions } from "typeorm/driver/mysql/MysqlConnectionOptions"
+export const MysqlConfig: MysqlConnectionOptions={
+    type:'mysql',
+    host:'localhost',
+    port:3306,
+    username:'root',
+    password:'',
+    database:'nest-tp'
+}
